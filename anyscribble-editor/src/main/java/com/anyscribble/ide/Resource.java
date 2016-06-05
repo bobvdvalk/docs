9f6593475c0ccd4e905031491ba96c1462a29334
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-06-05 14:34:32 +0200

    Clean license headers

--- anyscribble-editor/src/main/java/com/anyscribble/ide/Resource.java
+++ anyscribble-editor/src/main/java/com/anyscribble/ide/Resource.java
@@ -1,17 +1,17 @@
 /**
  * AnyScribble Editor - Writing for Developers by Developers
  * Copyright © 2016 Thomas Biesaart (thomas.biesaart@gmail.com)
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -78,7 +78,7 @@ public class Resource {
     private static String getVersion() {
         String version = AnyScribble.class.getPackage().getImplementationVersion();
         if (version == null) {
-            version = "Development";
+            version = "Development Version";
         }
         return version;
     }
