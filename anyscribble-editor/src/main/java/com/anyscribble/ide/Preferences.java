db036df5f1ce0598ca9edcbe98018520668eccbd
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-06-10 20:57:03 +0200

    Add documentation for Preferences

--- anyscribble-editor/src/main/java/com/anyscribble/ide/Preferences.java
+++ anyscribble-editor/src/main/java/com/anyscribble/ide/Preferences.java
@@ -1,17 +1,17 @@
 /**
  * AnyScribble Editor - Writing for Developers by Developers
  * Copyright © 2016 Thomas Biesaart (thomas.biesaart@gmail.com)
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -38,6 +38,15 @@ import java.util.regex.Pattern;
 
 /**
  * This class is responsible for persisting simple preferences.
+ * It works by identifying a preference which is stored as a string.
+ * For example, I want to save the last entered message in my dialog. Let's call this preference dialog.message.
+ * <pre>
+ * // Save my message
+ * preferences.put("dialog.message", "Hello World");
+ *
+ * // Fetch my message and specify a default
+ * preferences.get("dialog.message").orElse("This is the first time you see this dialog");
+ * </pre>
  *
  * @author Thomas Biesaart
  */
@@ -92,7 +101,7 @@ public class Preferences {
 
     }
 
-    public synchronized void sync() {
+    private synchronized void sync() {
         LOGGER.debug("Reading properties from {}", targetFile);
         saveTimeline.stop();
         if (Files.exists(targetFile)) {
@@ -104,20 +113,47 @@ public class Preferences {
         }
     }
 
+    /**
+     * Save a String preference.
+     * Use this method to save a preference that is identified by a key.
+     *
+     * @param key   the unique key of the preference
+     * @param value the value that should be saved
+     * @throws NullPointerException if the key or value is null
+     */
     public synchronized void put(String key, String value) {
         properties.put(key, value);
         saveTimeline.playFromStart();
         LOGGER.debug("Put preference {} = {}", key, value);
     }
 
+    /**
+     * Save a collection of objects. The string value of these objects will be saved to the persistence.
+     * Fetch it later using {@link #getList(String)}.
+     *
+     * @param key   the unique key of the preference
+     * @param value the collection
+     */
     public synchronized void putList(String key, Iterable<?> value) {
         put(key, StringUtils.join(value, File.pathSeparator));
     }
 
+    /**
+     * Fetch a preference from the persistence.
+     *
+     * @param key the unique key of the preference
+     * @return the preference of {@link Optional#empty()}} if it was not found
+     */
     public Optional<String> get(String key) {
         return Optional.ofNullable(properties.getProperty(key));
     }
 
+    /**
+     * Fetch a collection preference from the persistence.
+     *
+     * @param key the unique key of the preference
+     * @return the preference of {@link Optional#empty()}} if it was not found
+     */
     public Optional<List<String>> getList(String key) {
         String value = get(key).orElse(null);
         if (value == null) {
